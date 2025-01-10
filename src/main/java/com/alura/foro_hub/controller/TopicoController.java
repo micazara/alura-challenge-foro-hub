60abbff8366915ba6d8cd529f13b3c4f5e1643b4
Mica Zara <zara_micaela@outlook.com>
2025-01-10 00:10:49 -0300

    eliminar un topico

--- src/main/java/com/alura/foro_hub/controller/TopicoController.java
+++ src/main/java/com/alura/foro_hub/controller/TopicoController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,14 +16,13 @@ import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.data.domain.*;
 import org.springframework.data.web.PageableDefault;
 
-import org.springframework.http.ResponseEntity;
-
 import com.alura.foro_hub.domain.topico.Topico;
 import com.alura.foro_hub.domain.topico.TopicoRepository;
 import com.alura.foro_hub.domain.topico.TopicoService;
 import com.alura.foro_hub.domain.topico.dto.DatosRespuestaTopico;
 import com.alura.foro_hub.domain.topico.dto.DatosRegistroTopico;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 
 @RestController
@@ -52,6 +52,18 @@ public class TopicoController {
     public ResponseEntity<DatosRespuestaTopico> detallarTopico(@PathVariable Long id) {
         Topico topico = topicoRepository.getReferenceById(id);
         return ResponseEntity.ok(new DatosRespuestaTopico(topico));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity eliminar(@PathVariable Long id) {
+        Optional<Topico> topico = topicoRepository.findById(id);
+        if (topico.isPresent()) {
+            topicoRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            throw new EntityNotFoundException();
+        }
+
     }
 
 }
